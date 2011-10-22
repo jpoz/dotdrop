91ee35a97798f8d3130bdd19511a9b6e54ab11a3
James Pozdena <jpoz@jpoz.net>
2011-10-22 00:06:38 -0700

    adding base classes

---
+++ lib/dotdrop.rb
@@ -0,0 +1,126 @@
+require 'logger'
+require "highline/import"
+require 'fileutils'
+
+
+class DotDrop
+
+  def self.run!(mode='add')
+    case mode
+    when 'add'
+      self.add
+    when 'import'
+      self.import
+    else
+      say("Could not understand #{mode}. Try add or import")
+    end
+  end
+
+  def self.add
+    say("Searching for Dropbox")
+
+    @dropbox_location = Dir.home + "/Dropbox"
+    while(!Dir.exist?(@dropbox_location))
+      @dropbox_location = ask("Couldn't find your Dropbox folder? There is no #{@dropbox} folder. Where is your Dropbox located?")
+    end
+
+    @dotfile_location = @dropbox_location + "/dotfiles"
+
+    unless Dir.exists?(@dotfile_location)
+      say("creating #{@dotfile_location}")
+      Dir::mkdir(@dotfile_location)
+    end
+
+    say("seaching for dotfiles")
+
+    Dir::glob(Dir.home + "/.?*").each do |target|
+      Addable.new(target).run
+    end
+  end
+
+  def self.dropbox_location
+    @dropbox_location
+  end
+
+  def self.dotfile_location
+    @dotfile_location
+  end
+
+
+  class Addable
+
+    attr_accessor :target, :destination, :name
+
+    BAD_TARGETS = [".","dropbox","DS_Store"]
+
+    def initialize(target, destination=nil)
+      @target = target
+      @destination = destination
+      /\.(?<name>[^\/]*)$/ =~ target
+      @name = name
+    end
+
+    def run
+      return unless self.viable_target?
+      if destination_exists?
+        say("Dropbox aready has a #{name}")
+        if File.symlink?(target)
+          loc = File.readlink(target)
+        end
+      else
+        unless File.symlink?(target)
+          if ask("Would you like to move <%= color('#{self.target}', :green) %> to your Dropbox?  ").downcase.include?("y")
+            self.install!
+          end
+        else
+          say("#{target} is a symlink")
+        end
+      end
+    end
+
+    def install!
+      say("\tCopying #{self.target} to #{self.destination}")
+      self.copy!(self.destination)
+      say("\tBacking up #{self.target} to #{self.backup_location}")
+      self.move!(self.backup_location)
+      say("\tCreating symlink #{self.target} to #{self.destination}")
+      self.symlink!(self.destination)
+    end
+
+    def copy!(des)
+      if File.directory?(self.target) 
+        FileUtils.mkdir(self.destination) unless Dir.exists?(des)
+        FileUtils.cp_r(self.target + "/.", des)
+      else
+        FileUtils.copy(self.target, des)
+      end
+    end
+
+    def move!(des)
+      FileUtils.mv(target, des)
+    end
+
+    def symlink!(des)
+      File.symlink(des, target)
+    end
+
+    def viable_target?
+      !BAD_TARGETS.include?(name)
+    end
+
+    def destination
+      return @destination if @destination
+      DotDrop.dotfile_location + "/" + name
+    end
+
+    def backup_location
+      self.target + "_backup"
+    end
+
+    def destination_exists?
+      File.exists?(self.destination)
+    end
+
+  end
+
+end
